e08cba6083aed1fc8e1118aef8adba328577cf62
Ryan Reece <ryan.d.reece@live.com>
2020-12-19 14:32:52 -0800

    ci hepplot in stats notes

--- python/hepplot/plot.py
+++ python/hepplot/plot.py
@@ -24,12 +24,16 @@ from matplotlib.collections import PatchCollection
 from matplotlib.patches import Rectangle
 from matplotlib.patches import Patch
 
-from . import stat
+from . import stats
 
 
-matplotlib.rc('text', usetex = True)
+#matplotlib.rc('text', usetex = True)
 #plt.style.use(hep.style.ATLAS)
 plt.style.use([hep.style.CMS, hep.style.firamath])
+plt.style.use([{
+    'xaxis.labellocation': 'center',
+    'yaxis.labellocation': 'center',
+    }])
 
 
 def hist1d(bins,
@@ -42,12 +46,25 @@ def hist1d(bins,
            data_label=None,
            signals=None,
            signal_labels=None,
+           signal_colors=None,
            xlabel=None,
            ylabel=None,
+           ycolors=None,
+           yalpha=None,
+           xlabels=None,
+           stacked=True,
+           xminorticks=True,
            unit=None,
            ratio=False,
-           ratio_label='Data / Model',
+           ratio_label=None,
            stack_signals=True,
+           xlim=None,
+           ylim=None,
+           yscale=None,
+           data_color='black',
+           figsize=None,
+           fontsize=None,
+           xlabels_fontsize=None,
            ):
 
     assert not ((yerr is not None) and (yerrs is not None))
@@ -65,20 +82,22 @@ def hist1d(bins,
             if labels is not None:
                 labels = labels.copy()
                 labels.reverse()
+            if ycolors is not None:
+                ycolors.reverse()
         else:
-            assert n_bins == len(y)
+            assert n_bins == len(y), "%i, %i" % (n_bins, len(y))
             n_samples = 1
             # if y one sample, and not a list of samples, make it one
             y = [y]
-            if not isinstance(labels, list):
+            if (labels is not None) and (not isinstance(labels, list)):
                 labels = [labels]
 
     ## set default poisson errors for data
     if (data is not None) and (data_err is None):
         data_err = np.zeros((n_bins, 2), dtype=np.float32)
         for j_bin in range(n_bins):
-            data_err[j_bin][0] = stat.poisson_error_up(data[j_bin])
-            data_err[j_bin][1] = stat.poisson_error_down(data[j_bin])
+            data_err[j_bin][0] = stats.poisson_error_up(data[j_bin])
+            data_err[j_bin][1] = stats.poisson_error_down(data[j_bin])
 
     ## convert to np.arrays if needed
     bins = np.asarray(bins, dtype=np.float32)
@@ -123,13 +142,13 @@ def hist1d(bins,
 
     ## allow gaussian symmetric data_err
     if data_err is not None:
-        if isinstance(data_err[0], (list, np.ndarray)):
+        if isinstance(data_err[0], (tuple, list, np.ndarray)):
             assert len(data_err[0]) == 2
         else:
             _data_err = np.zeros((n_bins, 2), dtype=np.float32)
             for j_bin in range(n_bins):
-                _data_err[j_bin][0] = _data_err[j_bin]
-                _data_err[j_bin][1] = _data_err[j_bin]
+                _data_err[j_bin][0] = data_err[j_bin]
+                _data_err[j_bin][1] = data_err[j_bin]
             data_err = _data_err
 
     ## convert yerr from [bin][up,down] to [down,up][bin]
@@ -157,15 +176,18 @@ def hist1d(bins,
 
     ## make top subplot
     fig = plt.figure()
-    axes = list()
     if (data is not None) and ratio:
         gs = fig.add_gridspec(2, 1, height_ratios=(3, 1),
                               wspace=0, hspace=0.04)
         ax1 = fig.add_subplot(gs[0, 0])
-        axes.append(ax1)
     else:
-        fig, ax1 = plt.subplots()
-        axes.append(ax1)
+        fig, ax1 = plt.subplots(figsize=figsize)
+
+    if yscale:
+        ax1.set_yscale('log')
+
+    axes = list()
+    axes.append(ax1)
 
     bincenters = np.mean(np.vstack([bins[0:-1],bins[1:]]), axis=0)
     binwidths = np.asarray([bins[i+1]-bins[i] for i in range(n_bins)], dtype=np.float32)
@@ -173,28 +195,52 @@ def hist1d(bins,
     ## plot hist stack
     if y is not None:
         # TODO: make colors configurable
-        if n_samples > 1:
+        if ycolors is not None:
+            colors=ycolors
+        elif n_samples > 1:
             colors = [plt.cm.Spectral(i/float(n_samples-1)) for i in range(n_samples)]
         else:
             colors = ['lightgray']
         weights = y
         binned = [np.asarray(bins[:-1], dtype=np.float32) for _ in range(n_samples)]
 
+        histtype='bar'
+        if not stacked:
+            histtype='stepfilled'
+
         plt.hist(binned, bins,
             weights=weights,
-            stacked=True,
+            stacked=stacked,
             density=False,
             color=colors,
             label=labels,
+            histtype=histtype,
+#            linewidth=2,
+#            linecolor=colors,
+            alpha=yalpha,
             )
 
-    # TODO
-    signal_colors = ['orange', 'cyan', 'lime', 'magenta']
+        if yalpha:
+            plt.hist(binned, bins,
+                weights=y,
+                stacked=False,
+                density=False,
+#                label=labels,
+                color=colors,
+                histtype='step',
+                linewidth=2,
+                fill=False,
+                )
+
+    if signal_colors is None:
+        signal_colors = ['orange', 'cyan', 'lime', 'magenta']
 
     ## plot signals
     n_signals = 0
     if signals is not None:
         n_signals = len(signals)
+        signal_colors = signal_colors[:n_signals] if signal_colors else None
+        signal_labels = signal_labels[:n_signals] if signal_labels else None
         if stack_signals: # stack signals on top of ytotal
             for i_sig in range(n_signals):
                 for j_bin in range(n_bins):
@@ -204,8 +250,8 @@ def hist1d(bins,
             weights=signals,
             stacked=False,
             density=False,
-            label=signal_labels[:n_signals],
-            color=signal_colors[:n_signals],
+            label=signal_labels,
+            color=signal_colors,
             histtype='step',
             linewidth=2,
             fill=False,
@@ -234,33 +280,54 @@ def hist1d(bins,
         plt.errorbar(bincenters, data, yerr=data_err, 
             label=data_label,
             fmt='o',
-            color='black',
-            ecolor='black',
+            color=data_color,
+            ecolor=data_color,
             elinewidth=2,
             capsize=0,
             markersize=8,
             zorder=100,
             )
-    
+
+    ## axis limits
+    if xlim is not None:
+        ax1.set_xlim(*xlim)
+    if ylim is not None:
+        ax1.set_ylim(*ylim)
+
+    if not xminorticks:
+        ax1.minorticks_off()
+
     ## axis labels
     _label = ''
     if ylabel:
         _label = ylabel
         if unit:
             _label += (' / (%g %s)' % (binwidths[0], unit))
-        plt.ylabel(_label)
+        plt.ylabel(_label, fontsize=fontsize) # HACK
     if not ratio:
         _label = ''
         if xlabel:
             _label = xlabel
             if unit:
                 _label += ' [%s]' % (unit)
-            plt.xlabel(_label)
+            plt.xlabel(_label, fontsize=fontsize) # HACK
+
+    if xlabels:
+        # We want to show all ticks...
+        ax1.set_xticks(np.arange(n_bins))
+        # ... and label them with the respective list entries.
+        ax1.set_xticklabels(xlabels, fontsize=xlabels_fontsize)
+        # Rotate the tick labels and set their alignment.
+        plt.setp(ax1.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+        # turn spines off
+#        for edge, spine in ax1.spines.items():
+#            spine.set_visible(False)
 
     ## make legend
     if labels or data_label:
         leg_handles, leg_labels = ax1.get_legend_handles_labels()
-    
+
         if (data is not None) and data_label:
             data_handle = leg_handles.pop()
             assert leg_labels.pop() == data_label
@@ -274,6 +341,9 @@ def hist1d(bins,
         if labels:
             leg_handles.reverse()
             leg_labels.reverse()
+        else:
+            leg_handles = list()
+            leg_labels = list()
 
         if yerr is not None:
             leg_handles.append(Patch(facecolor='darkgray',
@@ -293,9 +363,11 @@ def hist1d(bins,
         if ytotal is not None:
             sum_ytotal = sum(ytotal)
             total_mean = sum([y_i*x_i/sum_ytotal for y_i, x_i in zip(ytotal, bincenters)])
+            print('DEBUG: ytotal total_mean = ', total_mean, flush=True)
         elif data is not None:
             sum_data = sum(data)
             total_mean = sum([y_i*x_i/sum_data for y_i, x_i in zip(data, bincenters)])
+            print('DEBUG: data total_mean = ', total_mean, flush=True)
 
         middle_of_range = (bins[-1] - bins[0])/2
 
@@ -350,16 +422,21 @@ def hist1d(bins,
         if ratio_label:
             ax2.set_ylabel(ratio_label)
         #ax2.set_ylim(0.7, 1.3) # HACK
+        if not xminorticks:
+            ax2.minorticks_off()
+
         _label = ''
         if xlabel:
             _label = xlabel
             if unit:
                 _label += ' [%s]' % (unit)
             plt.xlabel(_label)
-    
+
         fig.subplots_adjust(wspace=0, hspace=0)
 
-    return fig, [ax1, ax2]
+    fig.align_labels()
+
+    return fig, axes
 
 
 def make_error_boxes(ax, xdata, ydata, xerror, yerror,
@@ -396,6 +473,161 @@ def make_error_boxes(ax, xdata, ydata, xerror, yerror,
     ax.add_collection(pc)
 
     return pc
+
+
+def make_heatmap(data, 
+                 xlabel=None,
+                 ylabel=None,
+                 xlabels=None,
+                 ylabels=None,
+                 cbar=False,
+                 cbar_kw=None, 
+                 cbar_label="",
+                 cbar_range=None,
+                 annotate=False, 
+                 figsize=None, 
+                 imshow=False,
+                 threshold=None,
+                 **kwargs):
+    """
+    Create a heatmap from a numpy array and two lists of labels.
+    Arguments:
+        data       : A 2D numpy array of shape (N,M)
+    Optional arguments:
+        xlabels    : A list or array of length M with the labels
+                     for the columns
+        ylabels    : A list or array of length N with the labels
+                     for the rows
+        cbar_kw    : A dictionary with arguments to
+                     :meth:`matplotlib.Figure.colorbar`.
+        cbar_label  : The label for the colorbar
+    All other arguments are directly passed on to the imshow call.
+    
+    From: https://matplotlib.org/gallery/images_contours_and_fields/image_annotated_heatmap.html
+    """
+    if figsize is None:
+        figsize = (12, 12)
+    
+    if cbar_kw is None:
+        cbar_kw = dict()
+    
+    if xlabels or ylabels:
+        imshow = True
+    
+    fig, ax = plt.subplots(figsize=figsize)
+
+    # Plot the heatmap
+    if imshow:
+        im = ax.imshow(data, **kwargs)
+    else:
+        im = ax.pcolormesh(data, **kwargs)
+
+    if cbar: # Create colorbar
+        cbar = fig.colorbar(im, ax=ax, **cbar_kw)
+        if cbar_range:
+            im.set_clim(*cbar_range)
+        if cbar_label:
+            cbar.ax.set_ylabel(cbar_label, rotation=-90, va="bottom", fontsize=16)
+
+    if xlabels:
+        # We want to show all ticks...
+        ax.set_xticks(np.arange(data.shape[1]))
+        # ... and label them with the respective list entries.
+        ax.set_xticklabels(xlabels, fontsize=14)
+    
+    if ylabels:
+        ax.set_yticks(np.arange(data.shape[0]))
+        ax.set_yticklabels(ylabels, fontsize=14)
+
+    if xlabels:
+        # Let the horizontal axes labeling appear on top.
+        ax.tick_params(top=True, bottom=False,
+                   labeltop=True, labelbottom=False)
+
+        # Rotate the tick labels and set their alignment.
+        plt.setp(ax.get_xticklabels(), rotation=-45, ha="right",
+             rotation_mode="anchor")
+
+        # Turn spines off and create white grid.
+        for edge, spine in ax.spines.items():
+            spine.set_visible(False)
+
+        ax.set_xticks(np.arange(data.shape[1]+1)-.5, minor=True)
+        ax.set_yticks(np.arange(data.shape[0]+1)-.5, minor=True)
+        ax.grid(which="minor", color="w", linestyle='-', linewidth=3)
+        ax.tick_params(which="minor", bottom=False, left=False)
+
+    if xlabel:
+        ax.set_xlabel(xlabel, fontsize=16)
+
+    if ylabel:
+        ax.set_ylabel(ylabel, fontsize=16)
+
+    if annotate:
+        texts = annotate_heatmap(im, data, valfmt="{x:.0f}", fontsize=14, threshold=threshold)
+
+    ax.tick_params(axis=u'both', which=u'both', length=0)
+    
+    fig.tight_layout()
+    
+    return fig, ax
+
+
+
+def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
+                     textcolors=["black", "white"],
+                     threshold=None, **textkw):
+    """
+    A function to annotate a heatmap.
+    Arguments:
+        im         : The AxesImage to be labeled.
+    Optional arguments:
+        data       : Data used to annotate. If None, the image's data is used.
+        valfmt     : The format of the annotations inside the heatmap.
+                     This should either use the string format method, e.g.
+                     "$ {x:.2f}", or be a :class:`matplotlib.ticker.Formatter`.
+        textcolors : A list or array of two color specifications. The first is
+                     used for values below a threshold, the second for those
+                     above.
+        threshold  : Value in data units according to which the colors from
+                     textcolors are applied. If None (the default) uses the
+                     middle of the colormap as separation.
+    Further arguments are passed on to the created text labels.
+    """
+    
+    shift = 0.0 # 0.5 # HACK
+
+    if not isinstance(data, (list, np.ndarray)):
+        data = im.get_array()
+
+    # Normalize the threshold to the images color range.
+    if threshold is not None:
+        threshold = im.norm(threshold)
+    else:
+        threshold = im.norm(data.max())/2.
+
+    # Set default alignment to center, but allow it to be
+    # overwritten by textkw.
+    kw = dict(horizontalalignment="center",
+              verticalalignment="center")
+    kw.update(textkw)
+
+    # Get the formatter in case a string is supplied
+    if isinstance(valfmt, str):
+        valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
+
+    # Loop over the data and create a `Text` for each "pixel".
+    # Change the text's color depending on the data.
+    texts = []
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            _n = im.norm(data[i, j])
+#            kw.update(color=textcolors[_n > threshold and _n < 0.95]) # HACK
+            kw.update(color=textcolors[_n >= threshold])
+            text = im.axes.text(j+shift, i+shift, valfmt(int(round(data[i, j])), None), **kw) # HACK
+            texts.append(text)
+
+    return texts
 
 
 def brazil(x, exp, obs,
