a5be452c5bd4c863781080238104c39a353df5be
Ryan Reece <ryan.d.reece@live.com>
2025-02-17 14:07:01 -0800

    update

--- python/covariance_calculators/estimators.py
+++ python/covariance_calculators/estimators.py
@@ -3,7 +3,7 @@ covariance_calculators/estimators.py
 
 TODO:
 
-[ ] Sample mean and covariance
+[x] Sample mean and covariance
 [ ] Rolling mean and covariance
 [ ] Exponential moving mean and covariance
 [ ] Online mean and covariance
@@ -14,14 +14,16 @@ TODO:
 import numpy as np
 
 
-def calc_sample_covariance(data, frequency=None):
-    assert False, "Not implemented"
+def calc_sample_mean(data, frequency=None):
+    _mean = np.mean(data, axis=1)
+    if not frequency is None:
+        _mean = _mean * frequency
+    return _mean
 
 
 def calc_sample_covariance(data, frequency=None):
     covariance = np.cov(data, rowvar=False, bias=False)  # bias=False uses n-1
-
-    # TODO: Use frequency
-
+    if not frequency is None:
+        covariance = covariance * frequency
     return covariance
 
