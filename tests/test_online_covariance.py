96b00477428579f384857244cbb158424a7697da
Ryan Reece <ryan.d.reece@live.com>
2025-03-07 00:38:00 -0800

    updating covariance_calculators submodule

--- tests/test_online_covariance.py
+++ tests/test_online_covariance.py
@@ -15,12 +15,12 @@ np.set_printoptions(precision=4, suppress=True)
 def test_online_covariance():
 
     # Generate sample data
-    n_samples = 1000
+    n_samples = 10000
     n_features = 3
-    true_mean = np.zeros(n_features)
+    true_mean = np.array([1, 2, 3])
     true_cov = np.array([[0.010,  0.005,  0.003],
-                         [0.005,  0.020, -0.002],
-                         [0.003, -0.002,  0.030]])
+                         [0.005,  0.020, -0.004],
+                         [0.003, -0.004,  0.040]])
     data = np.random.multivariate_normal(mean=true_mean,
                                    cov=true_cov,
                                    size=n_samples)
@@ -29,7 +29,7 @@ def test_online_covariance():
     covariance = calc_sample_covariance(data)
 
     assert np.allclose(mean, true_mean, rtol=0, atol=1e-2)
-    assert np.allclose(covariance, true_cov, rtol=0, atol=5e-3)
+    assert np.allclose(covariance, true_cov, rtol=0, atol=1e-3)
 
     online_calculator = OnlineCovariance(n_features)
 
