860aa2fbfc04ecd2c985689255e6a35349c35804
Ryan Reece <ryan.d.reece@live.com>
2025-03-07 18:55:34 -0800

    updating covariance_calculators submodule

--- tests/test_online_covariance.py
+++ tests/test_online_covariance.py
@@ -5,7 +5,7 @@ test_online_covariance.py
 import math
 import numpy as np
 
-from covariance_calculators.estimators import calc_sample_mean, calc_sample_covariance, OnlineCovariance
+from covariance_calculators.estimators import calc_sample_mean, calc_sample_covariance, OnlineCovariance, EMACovariance
 
 
 np.random.seed(42)
@@ -29,6 +29,7 @@ def test_online_covariance():
     mean = calc_sample_mean(data)
     covariance = calc_sample_covariance(data)
 
+    # Check that sample statistics are close to the truth
     assert np.allclose(mean, true_mean, rtol=0, atol=1e-2)
     assert np.allclose(covariance, true_cov, rtol=0, atol=1e-3)
 
@@ -38,65 +39,84 @@ def test_online_covariance():
     for row in data:
         online_calculator.add(row)
 
-    assert np.allclose(online_calculator.mean, mean, rtol=0, atol=1e-4)
-    assert np.allclose(online_calculator.cov, covariance, rtol=0, atol=1e-4)
+    # Check that online statistics are virtually identical to the sample statistics
+    assert np.allclose(online_calculator.mean, mean, rtol=0, atol=1e-5)
+    assert np.allclose(online_calculator.cov, covariance, rtol=0, atol=1e-5)
 
 
-def test_online_covariance_merge():
-    # create two differently correllated datasets
-    # (again, three dimensions)
-#    data_part1 = create_correlated_dataset( \
-#       500, (2.2, 4.4, 1.5), np.array([[0.2, 0.5, 0.7],[0.3, 0.2, 0.2],[0.5,0.3,0.1]]), (1, 5, 3))
-#   data_part2 = create_correlated_dataset( \
-#       1000, (5, 6, 2), np.array([[0.2, 0.5, 0.7],[0.3, 0.2, 0.2],[0.5,0.3,0.1]]), (1, 5, 3))
+def test_ema_covariance():
 
     # Generate sample data
     n_samples = 10000
     n_features = 3
     true_mean = np.array([1, 2, 3])
-    true_cov = np.array([[0.2, 2.5, 2.1],
-                         [0.3, 1. , 0.6],
-                         [0.5, 1.5, 0.3]])
-    data_part1 = np.random.multivariate_normal(
-                        mean=np.array([2.2, 4.4, 1.5]),  # true_mean
-                        cov=true_cov,
-                        size=500)  # n_samples
-    data_part2 = np.random.multivariate_normal(
-                        mean=np.array([5, 6, 2]),  # true_mean
-                        cov=true_cov,
-                        size=1000)  # n_samples
+    true_cov = np.array([[0.010,  0.005,  0.003],
+                         [0.005,  0.020, -0.004],
+                         [0.003, -0.004,  0.040]])
+    data = np.random.multivariate_normal(mean=true_mean,
+                                   cov=true_cov,
+                                   size=n_samples)
+    # Calculate EMA mean and covariance
+    ema_calculator = EMACovariance(n_features, alpha=0.001)
 
-    ocov_part1 = OnlineCovariance(3)
-    ocov_part2 = OnlineCovariance(3)
-    ocov_both = OnlineCovariance(3)
+    for row in data:
+        ema_calculator.add(row)
+
+    # Check that EMA statistics are nearly identical to the truth for small alpha
+    assert np.allclose(ema_calculator.mean, true_mean, rtol=0, atol=1e-2)
+    assert np.allclose(ema_calculator.cov, true_cov, rtol=0, atol=1e-3)
+
+
+def test_online_covariance_merge():
+
+    # Generate sample data with two differently correllated datasets
+    n_samples = 10000
+    n_features = 3
+    true_mean_1 = np.array([2.2, 4.4, 1.5])
+    true_mean_2 = np.array([5, 6, 2])
+    true_cov = np.array([[0.015,  0.008,  0.003],
+                         [0.008,  0.057, -0.021],
+                         [0.003, -0.021,  0.040]])
+    data_1 = np.random.multivariate_normal(
+                        mean=true_mean_1,
+                        cov=true_cov,
+                        size=int(n_samples/2))
+    data_2 = np.random.multivariate_normal(
+                        mean=true_mean_2,
+                        cov=true_cov,
+                        size=n_samples)
+
+    ocov_calc_1 = OnlineCovariance(n_features)
+    ocov_calc_2 = OnlineCovariance(n_features)
+    ocov_calc_both = OnlineCovariance(n_features)
     
-    # "grow" online-covariances for part 1 and 2 separately but also
+    # Save online-covariances for part 1 and 2 separately but also
     # put all observations into the OnlineCovariance object for both.
     
-    for row in data_part1:
-        ocov_part1.add(row)
-        ocov_both.add(row)
+    for row in data_1:
+        ocov_calc_1.add(row)
+        ocov_calc_both.add(row)
         
-    for row in data_part2:
-        ocov_part2.add(row)
-        ocov_both.add(row)
+    for row in data_2:
+        ocov_calc_2.add(row)
+        ocov_calc_both.add(row)
         
-    ocov_merged = ocov_part1.merge(ocov_part2)
+    ocov_calc_merged = ocov_calc_1.merge(ocov_calc_2)
     
-    assert ocov_both.count == ocov_merged.count, \
-    """
-    Count of ocov_both and ocov_merged should be the same.
-    """
-    assert np.isclose(ocov_both.mean, ocov_merged.mean).all(), \
-    """
-    Mean of ocov_both and ocov_merged should be the same.
-    """
-    assert np.isclose(ocov_both.cov, ocov_merged.cov).all(), \
-    """
-    Covarance-matrix of ocov_both and ocov_merged should be the same.
-    """
-    assert np.isclose(ocov_both.corr, ocov_merged.corr).all(), \
-    """
-    Pearson-Correlationcoefficient-matrix of ocov_both and ocov_merged should be the same.
-    """
+    assert ocov_calc_both.count == ocov_calc_merged.count, \
+        """
+        Count of both and merged should be the same.
+        """
+    assert np.allclose(ocov_calc_both.mean, ocov_calc_merged.mean), \
+        """
+        Mean of both and merged should be the same.
+        """
+    assert np.allclose(ocov_calc_both.cov, ocov_calc_merged.cov), \
+        """
+        Covarance-matrix of both and merged should be the same.
+        """
+    assert np.allclose(ocov_calc_both.corr, ocov_calc_merged.corr), \
+        """
+        Pearson-Correlationcoefficient-matrix of both and merged should be the same.
+        """
 
